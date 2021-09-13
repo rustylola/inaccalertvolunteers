a89dee8f6364912ed5e9dc42cc0c34348de3f109
rustylola <71629716+rustylola@users.noreply.github.com>
2021-09-14 01:03:44 +0800

    Main Activity adding button navigation/icons/online indicator

--- inaccalertvolunteers/MainActivity.cs
+++ inaccalertvolunteers/MainActivity.cs
@@ -5,13 +5,12 @@ using Android.Support.V7.App;
 
 namespace inaccalertvolunteers
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
+    [Activity(Label = "@string/app_name", Theme = "@style/logintheme", MainLauncher = false)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
